5545a662f275968e580fd9421c8946351fe516b7
biswapratap-healthcare <64803030+biswapratap-healthcare@users.noreply.github.com>
2022-05-19 23:48:42 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,13 +9,13 @@ import requests
 
 
 sqldb = SqlDatabase()
+print(sqldb.config)
 
 
-def is_image_selected(image_arr, style):
-    r = requests.post(sqldb.config['filter_host'] + ':' +
-                      sqldb.config['filter_port'] + '/filter_image?' +
-                      'style=' + style +
-                      'image=' + image_arr)
+def is_image_selected(image_str_arr, style):
+    print("Calling Prediction ...")
+    req = sqldb.config['filter_host'] + ':' + sqldb.config['filter_port'] + '/filter_image?style=' + style + '&image=' + image_str_arr
+    r = requests.post(req)
     d = json.loads(r.text)
     return d['result']
 
@@ -24,15 +24,16 @@ def search_image_fn(search_string, style):
     image_arrs = list(set([e[0] for e in sqldb.fetch_n_image_arrs(n=10)]))
     result = list()
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-        future_to_image_arr = {executor.submit(
-            is_image_selected, image_arr, style): image_arr for image_arr in image_arrs}
-        for future in concurrent.futures.as_completed(future_to_image_arr):
-            image_arr = future_to_image_arr[future]
+        future_to_image_str_arr = dict()
+        for image_arr in image_arrs:
+            image_str_arr = base64.b64encode(pickle.loads(image_arr)).decode("utf-8")
+            future_to_image_str_arr[executor.submit(is_image_selected, image_str_arr, style)] = image_str_arr
+        for future in concurrent.futures.as_completed(future_to_image_str_arr):
+            image_str_arr = future_to_image_str_arr[future]
             try:
                 res = future.result()
                 if res == "1":
-                    numpy_arr = pickle.loads(image_arr)
-                    result.append(base64.b64encode(numpy_arr).decode("utf-8"))
+                    result.append(image_str_arr)
             except Exception as exc:
                 print(exc)
     return result
